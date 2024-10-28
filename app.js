1a3766bc5829ff1f6abbf29d5d7e1c902a293c6e
dosabhijith <dosabhijith@gmail.com>
2024-10-28 12:54:18 +0530

    Create: Params / Qurey Params Routes

--- app.js
+++ app.js
@@ -1,25 +1,41 @@
 import http from "http";
-const port = 4000;
+import { URL } from "url";
+const port = 5000;
 
 const server = http.createServer((req, res) => {
   res.setHeader("Content-Type", "text/html");
-  if (req.url === "/") {
+  const url = new URL(req.url, `http://${req.headers.host}`);
+
+  // console.log(url);
+
+  if (url.pathname === "/") {
     res.write("<h1>Home Page</h1><p>This is the Home page</p>");
-  } else if (req.url === "/about") {
-    res.write("<h1>About</h1><p>This is the about page</p>");
-  } else if (req.url === "/service") {
-    
+  } else if (url.pathname.startsWith("/about")) {
+    const id = url.pathname.split("/")[2];
+    res.write(
+      `<h1>About</h1><p>This is the about page for ID No. ${
+        id ? id : "NOT GETTING ID"
+      }</p>`
+    );
+  } else if (url.pathname === "/service") {
     res.write("<h1>Services</h1><p>This is the services Page</p>");
-  } else if (req.url === "/contact") {
-    res.write("<h1>Contact</h1><p>This is the Contact Page</p>");
+  } else if (url.pathname.startsWith("/contact")) {
+    const name = url.searchParams.get("name");
+    res.write(
+      `<h1>Contact</h1><p>This is the Contact Page of ${
+        name ? name.toUpperCase() : "USER NOT FOUND"
+      }</p>`
+    );
   } else {
     res.writeHead(404);
-    res.write("<h1>404 Not Found</h1><p>The page you're looking for does not exist.</p>");
+    res.write(
+      "<h1>404 Not Found</h1><p>The page you're looking for does not exist.</p>"
+    );
   }
-  console.log(req.url);
-  console.log(req.headers);
-  console.log(req.method);
-  console.log(res.statusCode);
+  // console.log(req.url);
+  // console.log(req.headers);
+  // console.log(req.method);
+  // console.log(res.statusCode);
   res.end();
 });
 
